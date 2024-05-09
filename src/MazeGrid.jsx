d44b86f75ec5b402cba30a088c85c4a457e81ea4
tonicanada <toni.cm@gmail.com>
2024-05-08 22:43:15 -0400

    Añade búsqueda de camino mediante DFS

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function getFloorRandomNumber(size) {
@@ -14,31 +14,24 @@ function generatePath(start, end, boardSize) {
   const dy = [4, 6, 3, 6];
   const move1 = [0, dy[0]];
   let position = updatePosition(start, move1);
-  // console.log("position1", position);
 
   const move2 = [getFloorRandomNumber(boardSize - position[0]), 0];
   position = updatePosition(position, move2);
-  // console.log("position2", position);
 
   const move3 = [0, dy[1]];
   position = updatePosition(position, move3);
-  // console.log("position3", position);
 
   const move4 = [-getFloorRandomNumber(position[0]), 0];
   position = updatePosition(position, move4);
-  // console.log("position4", position);
 
   const move5 = [0, dy[2]];
   position = updatePosition(position, move5);
-  // console.log("position5", position);
 
   const move6 = [end[0] - position[0], 0];
   position = updatePosition(position, move6);
-  // console.log("position6", position);
 
   const move7 = [0, dy[3]];
   position = updatePosition(position, move7);
-  // console.log("position7", position);
 
   return [move1, move2, move3, move4, move5, move6, move7];
 }
@@ -51,19 +44,19 @@ function generateMaze() {
   // Establecer inicio y fin aleatorios en los bordes
   let start = [Math.floor(Math.random() * 20), 0];
   let end = [Math.floor(Math.random() * 20), 19];
-  maze[start[0]][start[1]] = "start";
- 
 
+  // Generamos un camino posible
   const moves = generatePath(start, end, 20);
-  console.log('start', start)
-  
-  const current = start;
+  // console.log("start", start);
+  maze[start[0]][start[1]] = "start";
 
-  console.log(moves)
+  const current = [...start];
+
+  // console.log(moves);
 
   for (let i = 0; i < moves.length; i++) {
     const move = moves[i];
-    console.log('move', move)
+    // console.log("move", move);
     if (move[0] == 0) {
       // Movimiento horizontal
       for (let j = 0; j < Math.abs(move[1]); j++) {
@@ -74,7 +67,6 @@ function generateMaze() {
         }
 
         maze[current[0]][current[1]] = "path";
-        console.log(current)
       }
     } else if (move[1] == 0) {
       // Movimiento vertical
@@ -86,13 +78,28 @@ function generateMaze() {
         }
         maze[current[0]][current[1]] = "path";
       }
-      console.log(current)
     }
   }
 
   maze[end[0]][end[1]] = "end";
-  console.log(maze)
-  return maze;
+
+  // Liberamos más celdas que sean del tipo "path"
+  const cellTypes = ["wall", "path"];
+
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze.length; j++) {
+      if (
+        maze[i][j] !== "path" &&
+        maze[i][j] !== "start" &&
+        maze[i][j] !== "end"
+      ) {
+        let randomIdx = Math.round(Math.random());
+        maze[i][j] = cellTypes[randomIdx];
+      }
+    }
+  }
+
+  return { maze, start, end };
 }
 
 // Función para renderizar el laberinto
@@ -106,8 +113,78 @@ const renderMaze = (maze) => {
   ));
 };
 
+const directions = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+];
+
+// Función helper para manejar el retraso
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const dfs = async (maze, start, visited, end, updateMaze) => {
+  console.log(start);
+  const newMaze = maze.map((row) => [...row]); // Crea una copia del laberinto para manipular
+  const currentCell = maze[start[0]][start[1]];
+
+  
+  if (currentCell != "start" && currentCell != "end") {
+    maze[start[0]][start[1]] = "visited";
+  }
+  updateMaze(newMaze);
+
+  await delay(100);  // Espera 100 ms antes de continuar
+
+  if (start[0] === end[0] && start[1] === end[1]) {
+    return true;
+  }
+
+  visited[start[0]][start[1]] = true;
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < directions.length; j++) {
+      let move = directions[j];
+      let newPosition = [start[0] + move[0], start[1] + move[1]];
+      let new_i = newPosition[0];
+      let new_j = newPosition[1];
+      if (new_i >= 0 && new_i < maze.length) {
+        if (new_j >= 0 && new_j < maze.length) {
+          if (!visited[new_i][new_j]) {
+            if (maze[new_i][new_j] != "wall") {
+              if (
+                await dfs(
+                  maze,
+                  [new_i, new_j],
+                  visited,
+                  end,
+                  updateMaze,
+                )
+              ) {
+                return true;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
+};
+
 function MazeGrid() {
-  const [maze, setMaze] = useState(generateMaze());
+  const [mazeDict, setMaze] = useState(generateMaze());
+  const { maze, start, end } = mazeDict;
+
+  const updateMaze = (newMaze) => {
+    setMaze({ ...mazeDict, maze: newMaze });
+  };
+
+  const handleSearchClick = async () => {
+    const visited = Array.from({ length: maze.length }, () =>
+      Array(maze.length).fill(false)
+    );
+    await dfs(maze, start, visited, end, updateMaze);
+  };
 
   const regenerateMaze = () => {
     setMaze(generateMaze()); // Generar y establecer un nuevo laberinto
@@ -117,6 +194,7 @@ function MazeGrid() {
     <div>
       {renderMaze(maze)}
       <button onClick={regenerateMaze}>Regenerar laberinto</button>
+      <button onClick={handleSearchClick}>Ejecutar DFS</button>
     </div>
   );
 }
